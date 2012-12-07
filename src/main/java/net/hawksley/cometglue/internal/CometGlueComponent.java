066d270546afdb86f1c1b3434bcdbae6c10cc51d
John Hawksley <john.hawksley@gmail.com>
2012-12-07 23:46:40 +0100

    Initial commit.

---
+++ src/main/java/net/hawksley/cometglue/internal/CometGlueComponent.java
@@ -0,0 +1,86 @@
+package net.hawksley.cometglue.internal;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.Properties;
+
+import javax.servlet.Servlet;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.ReferenceCardinality;
+import org.cometd.bayeux.server.BayeuxServer;
+import org.cometd.server.BayeuxServerImpl;
+import org.cometd.server.CometdServlet;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.log.LogService;
+
+@Component
+public class CometGlueComponent
+{
+
+    private static final String COMETD_ALIAS = "/cometd";
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected HttpService httpService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected LogService log;
+
+    private BayeuxServerImpl bayeux;
+    private ServiceRegistration serviceReg;
+
+    protected void activate(ComponentContext ctxt) throws Exception
+    {
+        Servlet servlet = new CometdServlet();
+        Dictionary<String, String> opts = new Hashtable<String, String>();
+
+        putEnvironmentalOpts(opts);
+
+        log.log(LogService.LOG_DEBUG, "Bayeux options: " + opts);
+
+        // Register the cometd service with the OSGi web service.
+        // In jetty.xml, the following conenctor must be used:
+        // <New class="org.eclipse.jetty.server.nio.SelectChannelConnector">
+        httpService.registerServlet(COMETD_ALIAS, servlet, opts, null);
+
+        bayeux = ((CometdServlet) servlet).getBayeux();
+
+        // Add our test channel listener
+        // bayeux.addListener(new CometGlueChannelListener(log));
+
+        serviceReg = ctxt.getBundleContext().registerService(
+                BayeuxServer.class.getName(), bayeux, null);
+
+        log.log(LogService.LOG_DEBUG, "Active and session open.");
+    }
+
+    protected void deactivate(ComponentContext ctx) throws Exception
+    {
+        httpService.unregister(COMETD_ALIAS);
+        bayeux.stop();
+        serviceReg.unregister();
+
+        log.log(LogService.LOG_DEBUG, "Deactivated.");
+    }
+
+    private void putEnvironmentalOpts(Dictionary<String, String> opts)
+    {
+        Properties sysprops = System.getProperties();
+
+        String optPrefix = "bayeux.";
+
+        for (Object raw : sysprops.keySet())
+        {
+            String key = (String) raw;
+
+            if (key.startsWith(optPrefix))
+            {
+                opts.put(key.substring(optPrefix.length()),
+                        sysprops.getProperty(key));
+            }
+        }
+    }
+}
