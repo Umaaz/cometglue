6cca6f5afce3da05912389740b97ebf0d9065353
John Hawksley <john.hawksley@gmail.com>
2013-03-03 16:23:22 +0100

    Changes formatting to standard Java conventions - makes the code more compact.

--- src/main/java/net/hawksley/cometglue/internal/CometGlueComponent.java
+++ src/main/java/net/hawksley/cometglue/internal/CometGlueComponent.java
@@ -18,8 +18,7 @@ import org.osgi.service.http.HttpService;
 import org.osgi.service.log.LogService;
 
 @Component
-public class CometGlueComponent
-{
+public class CometGlueComponent {
     private static final String COMETD_ALIAS = "/cometd";
 
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
@@ -31,58 +30,55 @@ public class CometGlueComponent
     private BayeuxServerImpl bayeux;
     private ServiceRegistration serviceReg;
 
-    protected void activate(ComponentContext ctxt) throws Exception
-    {
-        Servlet servlet = new CometdServlet();
+    protected void activate(ComponentContext ctxt) throws Exception {
+	Servlet servlet = new CometdServlet();
 
-        // Read the options out of the environment.
-        Dictionary<String, String> opts = new Hashtable<String, String>();
-        putEnvironmentalOpts(opts);
+	// Read the options out of the environment.
+	Dictionary<String, String> opts = new Hashtable<String, String>();
+	putEnvironmentalOpts(opts);
 
-        if (log != null)
-            log.log(LogService.LOG_DEBUG, "Using Bayeux options: " + opts);
+	if (log != null)
+	    log.log(LogService.LOG_DEBUG, "Using Bayeux options: " + opts);
 
-        // Register the cometd service with the OSGi web service.
-        // In jetty.xml, the following connector must be used:
-        // <New class="org.eclipse.jetty.server.nio.SelectChannelConnector">
-        httpService.registerServlet(COMETD_ALIAS, servlet, opts, null);
+	// Register the cometd service with the OSGi web service.
+	// In jetty.xml, the following connector must be used:
+	// <New class="org.eclipse.jetty.server.nio.SelectChannelConnector">
+	httpService.registerServlet(COMETD_ALIAS, servlet, opts, null);
 
-        bayeux = ((CometdServlet) servlet).getBayeux();
+	bayeux = ((CometdServlet) servlet).getBayeux();
 
-        // Put Bayeux into the service registry.
-        serviceReg = ctxt.getBundleContext().registerService(BayeuxServer.class.getName(), bayeux, null);
+	// Put Bayeux into the service registry.
+	serviceReg = ctxt.getBundleContext().registerService(
+		BayeuxServer.class.getName(), bayeux, null);
 
-        if (log != null)
-            log.log(LogService.LOG_DEBUG, "Component active.");
+	if (log != null)
+	    log.log(LogService.LOG_DEBUG, "Component active.");
     }
 
     // Deactivate the service - stops Bayeux and unregisters the service.
-    protected void deactivate(ComponentContext ctx) throws Exception
-    {
-        httpService.unregister(COMETD_ALIAS);
-        bayeux.stop();
-        serviceReg.unregister();
+    protected void deactivate(ComponentContext ctx) throws Exception {
+	httpService.unregister(COMETD_ALIAS);
+	bayeux.stop();
+	serviceReg.unregister();
 
-        if (log != null)
-            log.log(LogService.LOG_DEBUG, "Component deactivated.");
+	if (log != null)
+	    log.log(LogService.LOG_DEBUG, "Component deactivated.");
     }
 
     // Looks for keys starting with "bayeux" and strips that, putting the
     // remainder into the passed dictionary.
-    private void putEnvironmentalOpts(Dictionary<String, String> opts)
-    {
-        Properties sysprops = System.getProperties();
+    private void putEnvironmentalOpts(Dictionary<String, String> opts) {
+	Properties sysprops = System.getProperties();
 
-        String optPrefix = "bayeux.";
+	String optPrefix = "bayeux.";
 
-        for (Object raw : sysprops.keySet())
-        {
-            String key = (String) raw;
+	for (Object raw : sysprops.keySet()) {
+	    String key = (String) raw;
 
-            if (key.startsWith(optPrefix))
-            {
-                opts.put(key.substring(optPrefix.length()), sysprops.getProperty(key));
-            }
-        }
+	    if (key.startsWith(optPrefix)) {
+		opts.put(key.substring(optPrefix.length()),
+			sysprops.getProperty(key));
+	    }
+	}
     }
 }
